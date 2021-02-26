d3412ee865e9e680e82112a5c563a6a1fa0ddae8
Freek van de Ven <FJM.vandeVen@student.han.nl>
2021-02-26 11:31:26 +0100

    exercise 5,6,7,8

--- src/main/java/nl/han/dea/ItemResource.java
+++ src/main/java/nl/han/dea/ItemResource.java
@@ -1,13 +1,22 @@
 package nl.han.dea;
 
+import nl.han.dea.services.ItemService;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/items")
 public class ItemResource {
+
+    private ItemService itemService;
+
+    public ItemResource() {
+        this.itemService = new ItemService();
+    }
 
     @GET
     @Path("/")
@@ -20,6 +29,17 @@ public class ItemResource {
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getItemsJson() {
-        return Response.ok("[\"bread\", \"butter\"]").build();
+        return Response.status(Response.Status.OK)
+                .entity(itemService.getAll())
+                .build();
+    }
+
+    @GET
+    @Path("/{itemID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getItemById(@PathParam("itemID") Integer itemID) {
+        return Response.status(Response.Status.OK)
+                .entity(itemService.getItem(itemID))
+                .build();
     }
 }
